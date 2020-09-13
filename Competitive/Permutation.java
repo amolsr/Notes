0cbda818ed69a14aa917acca753b9c609ca796fd
Amol Saini <38129418+amolsr@users.noreply.github.com>
2020-09-13 22:10:48 +0530

    Update Permutation.java

--- Competitive/Permutation.java
+++ Competitive/Permutation.java
@@ -23,6 +23,25 @@ public class Permutation {
             permutations(consChars + input.charAt(i), input.substring(0, i) + input.substring(i + 1), list);
         }
     }
+    
+    public <E> List<List<E>> generatePerm(List<E> original) {
+     if (original.isEmpty()) {
+       List<List<E>> result = new ArrayList<>(); 
+       result.add(new ArrayList<>()); 
+       return result; 
+     }
+     E firstElement = original.remove(0);
+     List<List<E>> returnValue = new ArrayList<>();
+     List<List<E>> permutations = generatePerm(original);
+     for (List<E> smallerPermutated : permutations) {
+       for (int index=0; index <= smallerPermutated.size(); index++) {
+         List<E> temp = new ArrayList<>(smallerPermutated);
+         temp.add(index, firstElement);
+         returnValue.add(temp);
+       }
+     }
+     return returnValue;
+   }
 
     public static void main(String a[]) {
         ArrayList<Integer> intList = new ArrayList<Integer>();
